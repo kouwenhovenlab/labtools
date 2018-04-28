31bb2c0e91f7dad116149619fe1d7157c31eac3f
Wolfgang Pfaff <wopfa@microsoft.com>
2018-04-28 16:55:31 +0200

    better defaults.

--- labtools/mplplots/init_nb_plotting.py
+++ labtools/mplplots/init_nb_plotting.py
@@ -6,3 +6,7 @@ rcParams['savefig.dpi'] = 150
 rcParams['savefig.transparent'] = False
 rcParams['font.family'] = 'Arial'
 rcParams['font.size'] = 8
+rcParams['lines.markersize'] = 4
+rcParams['axes.facecolor'] = 'w'
+rcParams['figure.facecolor'] = 'w'
+rcParams['nbagg.transparent'] = False
